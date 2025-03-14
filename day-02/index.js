a3049a52daf86e756b7c91293e1887a2ca98f185
Manoj <manojkumarbonala3@gmail.com>
2025-03-14 07:34:02 +0530

    day-02

---
+++ day-02/index.js
@@ -0,0 +1,35 @@
+console.log("Day 2");
+
+var address = "Bangalore";
+console.log(address);
+
+var address = "USA";
+console.log(address);
+
+let studentName = "Kumar";
+let age = 24;
+let isStudent = false;
+let favouriteProgammingLanguage = "JavaScript";
+
+console.log(
+  studentName + " " + age + " " + isStudent + " " + favouriteProgammingLanguage
+);
+
+// Object:
+
+let employee = {
+  name: "Kumar",
+  salary: 120000,
+  department: "IT",
+};
+
+let details = employee
+console.log(details)
+
+
+// arrays:
+
+let numbers = [12, 3, 4, 5, 6, 9];
+
+let array = numbers
+console.log(array)
